3a958780e45d804bc70fd38cd45d9e2c4d7e436a
given-dragon <yijy001@naver.com>
2025-02-03 18:37:42 +0900

    [Feat] SwaggerConfig 생성, 추후 API 테스트를 위한 accessToken 설정 추가

---
+++ src/main/kotlin/com/whatever/config/SwaggerConfig.kt
@@ -0,0 +1,43 @@
+package com.whatever.config
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.info.Info
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.security.SecurityRequirement
+import io.swagger.v3.oas.models.security.SecurityScheme
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@OpenAPIDefinition(
+    info = Info(
+        title = "Caramel API Doc",
+        description = "Caramel API 문서입니다.",
+        version = "v1"
+    )
+)
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun openApi(): OpenAPI {
+
+        val jwtScheme = "Caramel Access Token"
+        val securityRequirement = SecurityRequirement().addList(jwtScheme)
+
+        val securityScheme = SecurityScheme()
+            .name(jwtScheme)
+            .type(SecurityScheme.Type.HTTP)
+            .`in`(SecurityScheme.In.HEADER)
+            .scheme("Bearer")
+            .bearerFormat("JWT")
+
+        val components = Components().addSecuritySchemes(jwtScheme, securityScheme)
+
+        return OpenAPI()
+            .addSecurityItem(securityRequirement)
+            .components(components)
+
+    }
+
+}
