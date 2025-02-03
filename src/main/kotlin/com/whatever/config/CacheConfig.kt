4cf8567450f2d0e072e5f5f0d6a0f85a8bb6db95
given-dragon <yijy001@naver.com>
2025-02-04 01:30:05 +0900

    [Feat] Redis Cache Config 추가

--- src/main/kotlin/com/whatever/config/CacheConfig.kt
+++ src/main/kotlin/com/whatever/config/CacheConfig.kt
@@ -9,7 +9,39 @@ import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cache.caffeine.CaffeineCacheManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
 import java.time.Duration
+
+@ConditionalOnProperty(
+    name = ["spring.cache.type"],
+    havingValue = "redis",
+    matchIfMissing = false
+)
+@EnableCaching
+@Configuration
+class RedisCacheConfig {
+
+    @Bean(name = ["oidcCacheManager"])
+    fun oidcCacheManager(redisConnectionFactory: RedisConnectionFactory): CacheManager {
+        val stringSerializationPair = RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer())
+        val jsonSerializerPair = RedisSerializationContext.SerializationPair.fromSerializer(GenericJackson2JsonRedisSerializer())
+
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(stringSerializationPair)
+            .serializeValuesWith(jsonSerializerPair)
+            .entryTtl(CacheType.OIDC_PUBLIC_KEY.ttl)
+
+        return RedisCacheManager.builder(redisConnectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build()
+    }
+
+}
 
 @ConditionalOnProperty(
     name = ["spring.cache.type"],
