17c1d60a028dcac93bc250eefe2f224b526260f0
evergreen <tkdfhr7497@naver.com>
2025-02-09 18:14:32 +0900

    [Fix] Kakao Login Header에 Bearer Prefix 추가

--- src/main/kotlin/com/whatever/config/OpenFeignConfig.kt
+++ src/main/kotlin/com/whatever/config/OpenFeignConfig.kt
@@ -1,8 +1,6 @@
 package com.whatever.config
 
-import feign.FeignException
-import feign.Logger
-import feign.Response
+import feign.*
 import feign.codec.Encoder
 import feign.codec.ErrorDecoder
 import feign.form.FormEncoder
@@ -20,6 +18,21 @@ class FeignClientGlobalConfig {
 }
 
 class KakaoOAuthConfig {
+
+    @Bean
+    fun requestInterceptor(): RequestInterceptor {
+        return RequestInterceptor { template: RequestTemplate ->
+            template.headers()["Authorization"]
+                ?.firstOrNull()
+                ?.takeIf { it.startsWith("Bearer ").not() }
+                ?.let {
+                    template.run {
+                        removeHeader("Authorization")
+                        header("Authorization", "Bearer $it")
+                    }
+                }
+        }
+    }
 
     @Bean
     fun formEncoder(): Encoder {
