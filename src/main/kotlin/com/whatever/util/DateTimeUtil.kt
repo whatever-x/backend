ccb5eb865579bf3517ffc4577234ef48d5dbeb40
given-dragon <yijy001@gmail.com>
2025-02-09 15:39:38 +0900

    [Refactor] DateTimeUtil 객체를 싱글톤객체로 전환

--- src/main/kotlin/com/whatever/util/DateTimeUtil.kt
+++ src/main/kotlin/com/whatever/util/DateTimeUtil.kt
@@ -5,43 +5,41 @@ import java.time.ZoneId
 import java.time.ZonedDateTime
 import java.util.*
 
-class DateTimeUtil {
-    companion object {
-        val UTC_ZONE_ID: ZoneId = ZoneId.of("UTC")
+object DateTimeUtil {
+    val UTC_ZONE_ID: ZoneId = ZoneId.of("UTC")
 
-        fun localNow(zoneId: ZoneId = UTC_ZONE_ID): LocalDateTime {
-            return LocalDateTime.now(zoneId)
-        }
+    fun localNow(zoneId: ZoneId = UTC_ZONE_ID): LocalDateTime {
+        return LocalDateTime.now(zoneId)
+    }
 
-        fun zonedNow(zoneId: ZoneId = UTC_ZONE_ID): ZonedDateTime {
-            return ZonedDateTime.now(zoneId)
-        }
+    fun zonedNow(zoneId: ZoneId = UTC_ZONE_ID): ZonedDateTime {
+        return ZonedDateTime.now(zoneId)
+    }
 
-        fun changeTimeZone(
-            sourceLocalDateTime: LocalDateTime,
-            sourceZone: ZoneId,
-            targetZone: ZoneId = UTC_ZONE_ID,
-        ): LocalDateTime {
-            return ZonedDateTime.of(sourceLocalDateTime, sourceZone)
-                .withZoneSameInstant(targetZone)
-                .toLocalDateTime()
-        }
+    fun changeTimeZone(
+        sourceLocalDateTime: LocalDateTime,
+        sourceZone: ZoneId,
+        targetZone: ZoneId = UTC_ZONE_ID,
+    ): LocalDateTime {
+        return ZonedDateTime.of(sourceLocalDateTime, sourceZone)
+            .withZoneSameInstant(targetZone)
+            .toLocalDateTime()
+    }
 
-        fun changeTimeZone(
-            sourceZonedDateTime: ZonedDateTime,
-            targetZone: ZoneId = UTC_ZONE_ID,
-        ): ZonedDateTime {
-            return sourceZonedDateTime
-                .withZoneSameInstant(targetZone)
-        }
+    fun changeTimeZone(
+        sourceZonedDateTime: ZonedDateTime,
+        targetZone: ZoneId = UTC_ZONE_ID,
+    ): ZonedDateTime {
+        return sourceZonedDateTime
+            .withZoneSameInstant(targetZone)
+    }
 
-        fun toDate(
-            sourceLocalDateTime: LocalDateTime,
-            sourceZone: ZoneId = UTC_ZONE_ID,
-        ): Date {
-            val instant = ZonedDateTime.of(sourceLocalDateTime, sourceZone).toInstant()
-            return Date.from(instant)
-        }
+    fun toDate(
+        sourceLocalDateTime: LocalDateTime,
+        sourceZone: ZoneId = UTC_ZONE_ID,
+    ): Date {
+        val instant = ZonedDateTime.of(sourceLocalDateTime, sourceZone).toInstant()
+        return Date.from(instant)
     }
 }
 
