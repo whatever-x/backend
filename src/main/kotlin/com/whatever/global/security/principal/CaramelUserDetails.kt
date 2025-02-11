fd38dbcbf87c7e23d896bb2487060669a1ba9fb0
given-dragon <yijy001@gmail.com>
2025-02-11 21:32:20 +0900

    [Feat] User 인증 객체 추가

---
+++ src/main/kotlin/com/whatever/global/security/principal/CaramelUserDetails.kt
@@ -0,0 +1,32 @@
+package com.whatever.global.security.principal
+
+import com.whatever.domain.user.dto.UserStatus
+import org.springframework.security.core.authority.SimpleGrantedAuthority
+import org.springframework.security.core.userdetails.UserDetails
+
+class CaramelUserDetails(
+    private val userId: Long,
+    private val status: UserStatus,
+) : UserDetails {
+
+    private val userIdStr = userId.toString()
+
+    override fun getAuthorities(): Set<SimpleGrantedAuthority> {
+        return setOf(
+            SimpleGrantedAuthority("ROLE_${status.name}"),
+        )
+    }
+
+    override fun getPassword(): String? {
+        return null
+    }
+
+    override fun getUsername(): String {
+        return userIdStr
+    }
+
+    fun getUserId(): Long {
+        return userId
+    }
+
+}
