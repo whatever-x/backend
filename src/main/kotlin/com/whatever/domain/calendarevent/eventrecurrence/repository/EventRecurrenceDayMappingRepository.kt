1bdae8a6f08c8f7afe619b4fbadd78ac28404938
given-dragon <yijy001@naver.com>
2025-02-22 03:41:01 +0900

    [Remove] 기존 이벤트 반복 관련 객체들 제거

--- src/main/kotlin/com/whatever/domain/calendarevent/eventrecurrence/repository/EventRecurrenceDayMappingRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.whatever.domain.calendarevent.eventrecurrence.repository
-
-import com.whatever.domain.calendarevent.eventrecurrence.model.EventRecurrenceDayMapping
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface EventRecurrenceDayMappingRepository : JpaRepository<EventRecurrenceDayMapping, Long> {
-}
