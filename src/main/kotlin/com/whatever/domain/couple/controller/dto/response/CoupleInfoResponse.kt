89b7317c88f00e284eab8cb3b737b744b3753ed9
given-dragon <yijy001@naver.com>
2025-03-04 03:07:51 +0900

    [Chore] Couple request&respones dto description 추가

--- src/main/kotlin/com/whatever/domain/couple/controller/dto/response/CoupleInfoResponse.kt
+++ src/main/kotlin/com/whatever/domain/couple/controller/dto/response/CoupleInfoResponse.kt
@@ -3,6 +3,7 @@ package com.whatever.domain.couple.controller.dto.response
 import io.swagger.v3.oas.annotations.media.Schema
 import java.time.LocalDate
 
+@Schema(description = "커플 상세 응답 모델")
 data class CoupleDetailResponse(
     val coupleId: Long,
     @Schema(description = "커플 시작일")
@@ -14,13 +15,14 @@ data class CoupleDetailResponse(
     val partnerInfo: CoupleUserInfoDto
 )
 
+@Schema(description = "커플 정보(유저 제외) 응답 모델")
 data class CoupleBasicResponse(
     val coupleId: Long,
     val startDate: LocalDate?,
     val sharedMessage: String?,
 )
 
-
+@Schema(description = "커플 유저 정보 모델")
 data class CoupleUserInfoDto(
     val id: Long,
     val nickname: String,
--- src/main/kotlin/com/whatever/domain/couple/controller/dto/response/CoupleInvitationCodeResponse.kt
+++ src/main/kotlin/com/whatever/domain/couple/controller/dto/response/CoupleInvitationCodeResponse.kt
@@ -1,5 +1,8 @@
 package com.whatever.domain.couple.controller.dto.response
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "커플 초대코드 응답 모델")
 data class CoupleInvitationCodeResponse(
     val invitationCode: String,
 )
