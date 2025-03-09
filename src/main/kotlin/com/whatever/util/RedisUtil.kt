7b32f4da9b200491567efc552b98e34eb21fa1cc
given-dragon <yijy001@naver.com>
2025-03-10 00:42:06 +0900

    [Feat] Couple Invitation Code를 위한 RedisUtil 메서드 추가

--- src/main/kotlin/com/whatever/util/RedisUtil.kt
+++ src/main/kotlin/com/whatever/util/RedisUtil.kt
@@ -3,6 +3,9 @@ package com.whatever.util
 import com.whatever.config.properties.JwtProperties
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.stereotype.Component
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.ZoneId
 import java.util.concurrent.TimeUnit
 
 @Component
@@ -13,6 +16,42 @@ class RedisUtil(
 
     companion object {
         private const val REFRESH_TOKEN_PREFIX = "token:refresh:"
+        private const val INVITATION_CODE_PREFIX = "couple:invitation:code"
+        private const val INVITATION_USER_PREFIX = "couple:invitation:user"
+    }
+
+    //  TODO(준용): Tx 추가
+    fun saveCoupleInvitationCode(
+        userId: Long,
+        invitationCode: String,
+        expirationTime: Duration = Duration.ofDays(1),
+    ): Boolean {
+        val invitationKey = "$INVITATION_CODE_PREFIX${invitationCode}"
+        val userKey = "$INVITATION_USER_PREFIX${userId}"
+
+        val userSaveResult = redisTemplate.opsForValue().setIfAbsent(invitationKey, "$userId", expirationTime) ?: false
+        val keySaveResult = redisTemplate.opsForValue().setIfAbsent(userKey, invitationCode, expirationTime) ?: false
+        return userSaveResult && keySaveResult
+    }
+
+    fun getCoupleInvitationCode(userId: Long): String? {
+        val userKey = "$INVITATION_USER_PREFIX${userId}"
+        return redisTemplate.opsForValue().get(userKey)
+    }
+
+    fun getCoupleInvitationUser(invitationCode: String): Long? {
+        val invitationKey = "$INVITATION_CODE_PREFIX${invitationCode}"
+        return redisTemplate.opsForValue().get(invitationKey)?.toLong()
+    }
+
+    fun getCoupleInvitationExpirationTime(
+        invitationCode: String,
+        zoneId: ZoneId = DateTimeUtil.UTC_ZONE_ID,
+    ): LocalDateTime? {
+        val invitationKey = "$INVITATION_CODE_PREFIX${invitationCode}"
+        return redisTemplate.getExpire(invitationKey)
+            .takeIf { it >= 0 }
+            ?.let { DateTimeUtil.localNow().plusSeconds(it) }
     }
 
     fun saveRefreshToken(
