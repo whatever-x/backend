ea5816c8ce97d19ae7f0a073549f505cdc15ee99
given-dragon <yijy001@naver.com>
2025-03-16 19:29:36 +0900

    [Refactor] Couple.users -> members로 변경 validateUser() 접근제어자와 메서드명 변경

--- src/main/kotlin/com/whatever/domain/couple/model/Couple.kt
+++ src/main/kotlin/com/whatever/domain/couple/model/Couple.kt
@@ -17,18 +17,18 @@ class Couple (
 ) : BaseEntity() {
 
     @OneToMany(mappedBy = "_couple", fetch = FetchType.LAZY)
-    protected val mutableUsers: MutableSet<User> = mutableSetOf()
-    val users: Set<User> get() = mutableUsers.toSet()
+    protected val mutableMembers: MutableSet<User> = mutableSetOf()
+    val members: Set<User> get() = mutableMembers.toSet()
 
     fun addUsers(user: User) {
-        mutableUsers.add(user)
+        mutableMembers.add(user)
     }
 
     @PrePersist
     @PreUpdate
-    fun validateUsers() {
-        if (mutableUsers.size != 2) {
-            throw IllegalStateException("커플에는 반드시 두 명의 유저가 있어야 합니다. 현재 등록된 유저 수: ${mutableUsers.size}")
+    protected fun validateMemberSize() {
+        if (mutableMembers.size != 2) {
+            throw IllegalStateException("커플에는 반드시 두 명의 유저가 있어야 합니다. 현재 등록된 유저 수: ${mutableMembers.size}")
         }
     }
 }
