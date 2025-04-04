1c0d1e27e0e6338d1f11748c3e91f5c5d4aadfe7
evergreen <tkdfhr7497@naver.com>
2025-04-05 00:27:18 +0900

    [Fix] 약관동의 validation 추가 및 사용하지 않는 Exception 제거

--- src/main/kotlin/com/whatever/domain/user/exception/UserExceptionCode.kt
+++ src/main/kotlin/com/whatever/domain/user/exception/UserExceptionCode.kt
@@ -9,11 +9,7 @@ enum class UserExceptionCode(
     override val status: HttpStatus = HttpStatus.BAD_REQUEST,
 ) : CaramelExceptionCode {
 
-    INVALID_NICKNAME_LENGTH("001", "닉네임은 2~10자 이내로 입력해주세요."),
     INVALID_NICKNAME_CHARACTER("002", "닉네임은 한글, 영문, 숫자만 사용 가능합니다."),
-    NICKNAME_REQUIRED("003", "닉네임은 필수 입력값입니다."),
-    SERVICE_TERMS_AGREEMENT_REQUIRED("005", "서비스 이용약관 동의가 필요합니다."),
-    PRIVATE_POLICY_AGREEMENT_REQUIRED("006", "개인정보 수집 및 이용 동의가 필요합니다."),
     ALREADY_EXIST_COUPLE("007", "이미 커플에 소속된 유저 입니다."),
     ;
 
