8c4676c653b4d363234db3db6c6b1e77f2bdb434
given-dragon <yijy001@naver.com>
2025-04-04 17:22:20 +0900

    [Chore] initial sql 파일을 git ignore에 등록

--- src/main/resources/initial-data-local.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO "user" ("birth_date", "is_deleted", "couple_id", "created_at", "id", "updated_at", "email", "gender", "nickname", "platform", "platform_user_id", "user_status")
-VALUES ('2025-03-03', false, null, '2025-03-06 02:07:45.000000', 1, '2025-03-06 02:07:49.000000', 'test@test.com', 'MALE', 'test user1', 'KAKAO', 'test-social-id', 'SINGLE');
