a56a21c47d7946abe2cb94352b9ac17063b5a246
given-dragon <yijy001@naver.com>
2025-04-07 00:52:53 +0900

    [Test] 로그인 시 OIDC 공개키가 만료되는 경우의 테스트코드 추가

--- src/test/kotlin/com/whatever/domain/auth/service/AuthServiceTest.kt
+++ src/test/kotlin/com/whatever/domain/auth/service/AuthServiceTest.kt
@@ -1,16 +1,33 @@
 package com.whatever.domain.auth.service
 
+import com.whatever.domain.auth.client.dto.KakaoIdTokenPayload
 import com.whatever.domain.auth.dto.ServiceToken
 import com.whatever.domain.auth.exception.AuthException
+import com.whatever.domain.auth.exception.AuthExceptionCode
+import com.whatever.domain.auth.exception.OidcPublicKeyMismatchException
+import com.whatever.domain.auth.service.provider.KakaoUserProvider
+import com.whatever.domain.auth.service.provider.SocialUserProvider
+import com.whatever.domain.user.model.LoginPlatform
+import com.whatever.domain.user.model.User
+import com.whatever.domain.user.model.UserStatus
+import com.whatever.domain.user.repository.UserRepository
 import com.whatever.global.security.util.SecurityUtil
 import com.whatever.util.RedisUtil
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.mockito.Mockito.*
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.doThrow
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.cache.Cache
+import org.springframework.cache.CacheManager
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.bean.override.mockito.MockitoBean
@@ -22,7 +39,13 @@ import kotlin.test.Test
 class AuthServiceTest @Autowired constructor(
     private val redisTemplate: RedisTemplate<String, String>,
     private val authService: AuthService,
+    private val userRepository: UserRepository,
 ) {
+    @MockitoBean
+    private lateinit var oidcCacheManager: CacheManager
+
+    @MockitoBean
+    private lateinit var oidcHelper: OIDCHelper
 
     @MockitoSpyBean
     private lateinit var redisUtil: RedisUtil
@@ -45,6 +68,7 @@ class AuthServiceTest @Autowired constructor(
         securityUtilMock.close()
         reset(redisUtil)
         reset(jwtHelper)
+        userRepository.deleteAllInBatch()
     }
 
     @DisplayName("유효하지 않은 리프레시 토큰이면 예외가 발생한다.")
@@ -75,5 +99,55 @@ class AuthServiceTest @Autowired constructor(
         // when, then
         assertThatThrownBy { authService.refresh(serviceToken) }
             .isInstanceOf(AuthException::class.java)
+    }
+
+    @DisplayName("캐시에 일치하는 oidc 공개키가 없다면 다시 로드 후 정상 흐름으로 동작한다.")
+    @Test
+    fun signUpOrSignIn_WithExpiredOidcPublicKey() {
+        // given
+        val oidcPublicKeyCacheName = "oidc-public-key"
+        val idToken = "idTokenWithPublicKeyIssue"
+        val exception = OidcPublicKeyMismatchException(AuthExceptionCode.ILLEGAL_KID)
+        val user = userRepository.save(User(
+            nickname = "testuser",
+            platform = LoginPlatform.KAKAO,
+            platformUserId = "test-kakao-user-id",
+            userStatus = UserStatus.SINGLE,
+        ))
+        val fakeKakaoIdTokenPayload = KakaoIdTokenPayload(
+            iss = "fake-kakao",
+            aud = "fake-kakao",
+            sub = user.platformUserId,
+            iat = 1000000L,
+            exp = 1000000L,
+            authTime = 1000000L,
+        )
+        val fakeServiceToken = ServiceToken(
+            accessToken = "test-access-token",
+            refreshToken = "test-refresh-token",
+        )
+
+        whenever(oidcHelper.parseKakaoIdToken(any(), any()))
+            .thenThrow(exception)  // Oidc PublicKey가 만료되어 불일치 발생
+            .thenReturn(fakeKakaoIdTokenPayload)  // 캐싱 후 다시 정상값 반환
+        whenever(jwtHelper.createAccessToken(user.id))
+            .thenReturn(fakeServiceToken.accessToken)
+        whenever(jwtHelper.createRefreshToken())
+            .thenReturn(fakeServiceToken.refreshToken)
+        whenever(oidcCacheManager.getCache(oidcPublicKeyCacheName))
+            .thenReturn(mock(Cache::class.java))
+
+
+        // when
+        val result = authService.signUpOrSignIn(
+            loginPlatform = user.platform,
+            idToken = idToken,
+        )
+
+        // then
+        verify(oidcCacheManager.getCache(oidcPublicKeyCacheName))!!.evictIfPresent(user.platform.name)
+        assertThat(result.nickname).isEqualTo(user.nickname)
+        assertThat(result.serviceToken.accessToken).isEqualTo(fakeServiceToken.accessToken)
+        assertThat(result.serviceToken.refreshToken).isEqualTo(fakeServiceToken.refreshToken)
     }
 }
