6a864a6a5e94d70c0485bf3e886d258ebfcbd743
evergreen <tkdfhr7497@naver.com>
2025-04-19 21:26:23 +0900

    [Fix] nickname이 null인 경우에는 검증하지 않도록 수정

--- src/main/kotlin/com/whatever/domain/user/dto/PutUserProfileRequest.kt
+++ src/main/kotlin/com/whatever/domain/user/dto/PutUserProfileRequest.kt
@@ -1,14 +1,15 @@
 package com.whatever.domain.user.dto
 
 import io.swagger.v3.oas.annotations.media.Schema
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Size
+import jakarta.validation.constraints.Pattern
 import java.time.LocalDate
 
 data class PutUserProfileRequest(
     @Schema(description = "닉네임")
-    @field:NotBlank(message = "닉네임은 공백일 수 없습니다.")
-    @field:Size(min = 3, max = 10, message = "닉네임은 3~10자 이내로 입력해주세요.")
+    @field:Pattern(
+        regexp = "^(?!\\s*$)[가-힣a-zA-Z0-9]{3,10}$",
+        message = "닉네임은 3~10자의 한글, 영문, 숫자로만 입력해주세요."
+    )
     val nickname: String?,
 
     @Schema(description = "생일")
