9f409298c2900f8751cb479135dddef6ac0c0ddc
given-dragon <yijy001@gmail.com>
2025-04-30 13:32:05 +0900

    [Refactor] SpecialDayApiExceptionCode HttpStatus 추가

--- src/main/kotlin/com/whatever/global/exception/externalserver/specialday/SpecialDayApiExceptionCode.kt
+++ src/main/kotlin/com/whatever/global/exception/externalserver/specialday/SpecialDayApiExceptionCode.kt
@@ -8,10 +8,10 @@ enum class SpecialDayApiExceptionCode(
     override val message: String,
     override val status: HttpStatus = HttpStatus.BAD_REQUEST,
 ) : ExternalServerExceptionCode {
-    UNKNOWN("000", "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해 주세요."),
-    EMPTY_HEADER("001", "확인할 수 없는 공휴일 정보 응답입니다."),
-    FAILED_RESPONSE_CODE("002", "공휴일 정보 요청에 실패했습니다."),
-    RESPONSE_TYPE_UNMATCHED("003", "알 수 없는 응답 타입입니다."),
+    UNKNOWN("000", "알 수 없는 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.", HttpStatus.INTERNAL_SERVER_ERROR),
+    EMPTY_HEADER("001", "확인할 수 없는 공휴일 정보 응답입니다.", HttpStatus.BAD_GATEWAY),
+    FAILED_RESPONSE_CODE("002", "공휴일 정보 요청에 실패했습니다.", HttpStatus.BAD_GATEWAY),
+    RESPONSE_TYPE_UNMATCHED("003", "알 수 없는 응답 타입입니다.", HttpStatus.BAD_GATEWAY),
     ;
 
     override val code: String
