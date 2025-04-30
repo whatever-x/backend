c95b22428ed295be72a7e5e71b060cad5d89c912
given-dragon <yijy001@naver.com>
2025-05-01 00:25:28 +0900

    [Feat] 밸런스 게임 Entity 생성

---
+++ src/main/kotlin/com/whatever/domain/balancegame/model/BalanceGame.kt
@@ -0,0 +1,23 @@
+package com.whatever.domain.balancegame.model
+
+import com.whatever.domain.base.BaseEntity
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import java.time.LocalDate
+
+@Entity
+class BalanceGame(
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long = 0L,
+
+    @Column(nullable = false, unique = true)
+    val gameDate: LocalDate,
+
+    @Column(nullable = false)
+    val question: String,
+
+) : BaseEntity()
