332808b2725bf5a3c2fe0f0501da05e9fb6092a9
given-dragon <yijy001@gmail.com>
2025-05-02 17:49:38 +0900

    [Refactor] BalanceGame.options 연관관계 추가, repository 조회 메서드 추가

--- src/main/kotlin/com/whatever/domain/balancegame/model/BalanceGame.kt
+++ src/main/kotlin/com/whatever/domain/balancegame/model/BalanceGame.kt
@@ -3,9 +3,11 @@ package com.whatever.domain.balancegame.model
 import com.whatever.domain.base.BaseEntity
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import java.time.LocalDate
 
 @Entity
@@ -19,5 +21,8 @@ class BalanceGame(
 
     @Column(nullable = false)
     val question: String,
+
+    @OneToMany(mappedBy = "balanceGame", fetch = FetchType.LAZY)
+    val options: List<BalanceGameOption> = listOf()
 
 ) : BaseEntity()
--- src/main/kotlin/com/whatever/domain/balancegame/repository/BalanceGameRepository.kt
+++ src/main/kotlin/com/whatever/domain/balancegame/repository/BalanceGameRepository.kt
@@ -1,7 +1,11 @@
 package com.whatever.domain.balancegame.repository
 
 import com.whatever.domain.balancegame.model.BalanceGame
+import com.whatever.util.DateTimeUtil
 import org.springframework.data.jpa.repository.JpaRepository
+import java.time.LocalDate
+import java.time.ZoneId
 
 interface BalanceGameRepository : JpaRepository<BalanceGame, Long> {
+    fun findByGameDateAndIsDeleted(gameDate: LocalDate, isDeleted: Boolean = false): BalanceGame?
 }
