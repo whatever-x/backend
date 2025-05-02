8e4175e22c8a29096dd267f344873e33cecb205f
given-dragon <yijy001@gmail.com>
2025-05-02 19:31:26 +0900

    [Feat] 밸런스 게임 선택지 선택 메서드 추가

---
+++ src/main/kotlin/com/whatever/domain/balancegame/controller/dto/request/ChooseBalanceGameOptionRequest.kt
@@ -0,0 +1,6 @@
+package com.whatever.domain.balancegame.controller.dto.request
+
+data class ChooseBalanceGameOptionRequest(
+    val gameId: Long,
+    val optionId: Long,
+)
--- src/main/kotlin/com/whatever/domain/balancegame/service/BalanceGameService.kt
+++ src/main/kotlin/com/whatever/domain/balancegame/service/BalanceGameService.kt
@@ -1,11 +1,14 @@
 package com.whatever.domain.balancegame.service
 
+import com.whatever.domain.balancegame.controller.dto.request.ChooseBalanceGameOptionRequest
+import com.whatever.domain.balancegame.controller.dto.response.ChooseBalanceGameOptionResponse
 import com.whatever.domain.balancegame.controller.dto.response.GetBalanceGameResponse
 import com.whatever.domain.balancegame.model.BalanceGame
 import com.whatever.domain.balancegame.model.UserChoiceOption
 import com.whatever.domain.balancegame.repository.BalanceGameRepository
 import com.whatever.domain.balancegame.repository.UserChoiceOptionRepository
 import com.whatever.domain.couple.repository.CoupleRepository
+import com.whatever.domain.user.repository.UserRepository
 import com.whatever.global.security.util.SecurityUtil
 import com.whatever.util.DateTimeUtil
 import org.springframework.stereotype.Service
@@ -17,7 +20,8 @@ import java.time.ZoneId
 class BalanceGameService(
     private val balanceGameRepository: BalanceGameRepository,
     private val userChoiceOptionRepository: UserChoiceOptionRepository,
-    private val coupleRepository: CoupleRepository
+    private val coupleRepository: CoupleRepository,
+    private val userRepository: UserRepository
 ) {
 
     companion object {
@@ -44,6 +48,48 @@ class BalanceGameService(
             options = sortedOptions,
             myChoice = memberChoices.find { it.user.id == SecurityUtil.getCurrentUserId() },
             partnerChoice = memberChoices.find { it.user.id != SecurityUtil.getCurrentUserId() },
+        )
+    }
+
+    @Transactional
+    fun chooseBalanceGameOption(request: ChooseBalanceGameOptionRequest): ChooseBalanceGameOptionResponse {
+        val balanceGame = getBalanceGame()
+        if (balanceGame.id != request.gameId) {
+            throw RuntimeException()  // TODO(준용) IAE-잘못된 게임 선택(게임 변경됨)
+        }
+
+        val coupleId = SecurityUtil.getCurrentUserCoupleId()
+        val requestUserId = SecurityUtil.getCurrentUserId()
+        val memberChoices = getCoupleMemberChoices(
+            coupleId = coupleId,
+            game = balanceGame,
+        )
+
+        val myChoice = memberChoices.find { it.user.id == requestUserId }
+        val partnerChoice = memberChoices.find { it.user.id != requestUserId }
+        if (myChoice != null) {
+            return ChooseBalanceGameOptionResponse.of(
+                game = balanceGame,
+                myChoice = myChoice,
+                partnerChoice = partnerChoice,
+            )
+        }
+        
+        val selectedOption = balanceGame.options.find { it.id == request.optionId }
+            ?: throw RuntimeException() // TODO(준용) IAE-잘못된 옵션 선택
+
+        val requestUser = userRepository.getReferenceById(requestUserId)
+        val newChoice = UserChoiceOption(
+            balanceGame = balanceGame,
+            balanceGameOption = selectedOption,
+            user = requestUser,
+        )
+        val savedChoice = userChoiceOptionRepository.save(newChoice)
+
+        return ChooseBalanceGameOptionResponse.of(
+            game = balanceGame,
+            myChoice = savedChoice,
+            partnerChoice = partnerChoice,
         )
     }
 
