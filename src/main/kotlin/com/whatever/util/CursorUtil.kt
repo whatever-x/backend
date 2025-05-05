cf8b47db0d63a770f59e432debc13446fa5ae3ec
evergreen <tkdfhr7497@naver.com>
2025-05-05 19:52:18 +0900

    [Fix] toString -> String 사용하여 메모리값 나오는 문제 수정

--- src/main/kotlin/com/whatever/util/CursorUtil.kt
+++ src/main/kotlin/com/whatever/util/CursorUtil.kt
@@ -1,21 +1,20 @@
 package com.whatever.util
 
+import java.nio.charset.StandardCharsets
 import java.util.*
 
 object CursorUtil {
     private const val SEPARATOR: String = "|"
 
     fun fromHash(cursorHash: String): List<String> {
-        return Base64.getUrlDecoder().decode(cursorHash).toString().split(SEPARATOR)
+        val decodedBytes = Base64.getUrlDecoder().decode(cursorHash)
+        val decodedString = String(decodedBytes, StandardCharsets.UTF_8)
+        return decodedString.split(SEPARATOR)
     }
 
     fun toHash(vararg cursors: Any): String {
         return cursors.joinToString(SEPARATOR) { it.toString() }
-            .let {
-                Base64.getUrlEncoder()
-                    .withoutPadding()
-                    .encodeToString(it.toByteArray())
-            }
-
+            .toByteArray(StandardCharsets.UTF_8)
+            .let { Base64.getUrlEncoder().withoutPadding().encodeToString(it) }
     }
 }
