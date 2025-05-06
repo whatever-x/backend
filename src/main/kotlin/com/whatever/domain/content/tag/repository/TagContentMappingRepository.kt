0f8284354ba4ffe1a8965ba353c22559a9647c31
given-dragon <yijy001@naver.com>
2025-05-07 03:31:23 +0900

    [Feat] 커플 나가기에 영향받는 entity의  bulk soft delete reposiroty 메서드 추가

--- src/main/kotlin/com/whatever/domain/content/tag/repository/TagContentMappingRepository.kt
+++ src/main/kotlin/com/whatever/domain/content/tag/repository/TagContentMappingRepository.kt
@@ -2,7 +2,9 @@ package com.whatever.domain.content.tag.repository
 
 import com.whatever.domain.content.tag.model.TagContentMapping
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
+import org.springframework.transaction.annotation.Transactional
 
 interface TagContentMappingRepository : JpaRepository<TagContentMapping, Long> {
 
@@ -15,4 +17,18 @@ interface TagContentMappingRepository : JpaRepository<TagContentMapping, Long> {
     fun findAllByContentIdWithTag(contentId: Long): List<TagContentMapping>
 
     fun findAllByContent_IdAndIsDeleted(contentId: Long, isDeleted: Boolean = false): List<TagContentMapping>
+
+    @Transactional
+    @Modifying(flushAutomatically = true, clearAutomatically = true)
+    @Query("""
+        update TagContentMapping tcm
+        set tcm.isDeleted = true
+        where tcm.id in (
+            select ttcm.id from TagContentMapping ttcm
+                join ttcm.content c
+            where c.user.id = :userId
+                and ttcm.isDeleted = false
+        )
+    """)
+    fun softDeleteAllByUserIdInBulk(userId: Long): Int
 }
