9255da8f20d342941e84377122f4741915e7f731
given-dragon <yijy001@naver.com>
2025-05-23 20:26:54 +0900

    [Feat] 커플 기념일 api 추가

---
+++ src/main/kotlin/com/whatever/domain/couple/controller/dto/response/CoupleAnniversaryResponse.kt
@@ -0,0 +1,55 @@
+package com.whatever.domain.couple.controller.dto.response
+
+import com.whatever.domain.couple.model.Couple
+import com.whatever.domain.couple.model.CoupleAnniversaryType
+import io.swagger.v3.oas.annotations.media.Schema
+import java.time.LocalDate
+
+@Schema(description = "커플 기념일 응답 모델")
+data class CoupleAnniversaryResponse(
+    val coupleId: Long,
+    val startDate: LocalDate?,
+    val sharedMessage: String?,
+
+    @Schema(description = "100일 단위 기념일")
+    val hundredDayAnniversaries: List<CoupleAnniversaryDto>,
+    @Schema(description = "1주년 단위 기념일")
+    val yearlyAnniversaries: List<CoupleAnniversaryDto>,
+    @Schema(description = "내 생일")
+    val myBirthDates: List<CoupleAnniversaryDto>,
+    @Schema(description = "상대방 생일")
+    val partnerBirthDates: List<CoupleAnniversaryDto>,
+) {
+    companion object {
+        fun of(
+            couple: Couple,
+            hundredDayAnniversaries: List<CoupleAnniversaryDto>,
+            yearlyAnniversaries: List<CoupleAnniversaryDto>,
+            myBirthDates: List<CoupleAnniversaryDto>,
+            partnerBirthDates: List<CoupleAnniversaryDto>,
+            ): CoupleAnniversaryResponse {
+            return CoupleAnniversaryResponse(
+                coupleId = couple.id,
+                startDate = couple.startDate,
+                sharedMessage = couple.sharedMessage,
+                hundredDayAnniversaries = hundredDayAnniversaries,
+                yearlyAnniversaries = yearlyAnniversaries,
+                myBirthDates = myBirthDates,
+                partnerBirthDates = partnerBirthDates,
+            )
+        }
+    }
+}
+
+data class CoupleAnniversaryDto(
+    @Schema(description = "커플 기념일의 종류")
+    val type: CoupleAnniversaryType,
+    @Schema(description = "커플 기념일")
+    val date: LocalDate,
+    @Schema(description = "커플 기념일 라벨")
+    val label: String,
+    @Schema(description =
+        "기념일이 원래 2월 29일이었으나, 해당 연도가 윤년이 아니어서 날짜가 조정되었는지 여부 (예: 2월 28일로 조정)"
+    )
+    val isAdjustedForNonLeapYear: Boolean = false,
+)
