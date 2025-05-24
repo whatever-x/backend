7006b425bae0fd604703e4f6380a709975077b27
given-dragon <yijy001@gmail.com>
2025-05-24 17:25:31 +0900

    [Refactor] FcmToken.updateToken()애서 기존 토큰과 동일하다면 updatedAt이 반영되지 않는 문제 수정

--- src/main/kotlin/com/whatever/domain/firebase/model/FcmToken.kt
+++ src/main/kotlin/com/whatever/domain/firebase/model/FcmToken.kt
@@ -3,6 +3,7 @@ package com.whatever.domain.firebase.model
 import com.whatever.domain.base.BaseEntity
 import com.whatever.domain.user.model.User
 import com.whatever.util.DateTimeUtil
+import com.whatever.util.DateTimeUtil.SYS_ZONE_ID
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.FetchType
@@ -11,6 +12,7 @@ import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
 import jakarta.persistence.JoinColumn
 import jakarta.persistence.ManyToOne
+import org.apache.catalina.core.ApplicationContext
 
 @Entity
 class FcmToken(
@@ -45,6 +47,10 @@ class FcmToken(
             throw IllegalArgumentException()  // TODO(준용) custom exception
         }
         _token = newToken
+
+        // newToken이 같더라도 updatedAt을 갱신하기위해 적용.
+        // 해당 시점의 updatedAt은 반영되지 않음.
+        updatedAt = DateTimeUtil.localNow(SYS_ZONE_ID)
     }
 
     fun isActiveToken(): Boolean {
