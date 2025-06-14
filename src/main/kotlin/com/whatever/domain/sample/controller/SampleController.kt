ad295bb1440dcd2f62c09bf2243f6a0152e7d94f
given-dragon <yijy001@naver.com>
2025-06-15 00:14:18 +0900

    [Fix] endpoint 인가 최신화, Sample endpoint prefix 추가

--- src/main/kotlin/com/whatever/domain/sample/controller/SampleController.kt
+++ src/main/kotlin/com/whatever/domain/sample/controller/SampleController.kt
@@ -22,6 +22,7 @@ import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 import java.time.LocalDate
@@ -32,6 +33,7 @@ import java.time.LocalDate
     description = "간단한 예제를 담은 샘플 API 입니다."
 )
 @RestController
+@RequestMapping("/sample")
 class SampleController(
     private val sampleService: SampleService,
 ) {
