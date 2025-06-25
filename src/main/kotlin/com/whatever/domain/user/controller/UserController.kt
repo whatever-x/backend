861e07ba9d19fa485d75157a4ba948cb8e32996d
given-dragon <yijy001@naver.com>
2025-06-25 17:55:15 +0900

    [Docs] User Controller에 관련된 api 문서 수정

--- src/main/kotlin/com/whatever/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/whatever/domain/user/controller/UserController.kt
@@ -11,13 +11,14 @@ import com.whatever.domain.user.service.UserService
 import com.whatever.global.exception.dto.CaramelApiResponse
 import com.whatever.global.exception.dto.succeed
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.web.bind.annotation.*
 
 @Tag(
-    name = "User",
-    description = "유저 API"
+    name = "유저 API",
+    description = "유저 프로필, 설정 등 유저 관련 기능을 제공하는 API"
 )
 @RestController
 @RequestMapping("/v1/user")
@@ -26,7 +27,15 @@ class UserController(
 ) {
     @Operation(
         summary = "프로필 수정",
-        description = "닉네임/생일을 수정합니다.",
+        description = """
+            ### 사용자의 닉네임과 생년월일을 수정합니다.
+            
+            - 수정하려는 값만 요청에 포함하면 됩니다.
+            - 수정하지 않을 값은 포함하지 않거나, `null`로 전송해야 합니다.
+        """,
+        responses = [
+            ApiResponse(responseCode = "200", description = "수정된 프로필 정보"),
+        ],
     )
     @PutMapping("/profile")
     fun updateProfile(
@@ -38,7 +47,16 @@ class UserController(
 
     @Operation(
         summary = "프로필 생성",
-        description = "약관 동의 여부와 함께 프로필을 생성합니다.",
+        description = """
+            ### 최초 로그인 후, 약관 동의 여부와 함께 프로필(닉네임, 생년월일)을 생성합니다.
+            
+            - 약관은 모두 동의되어야 합니다.
+            - 프로필을 생성한 유저는 SINGLE 상태로 전환됩니다.
+            - 프로필 생성 후, 알림 설정 API를 사용할 수 있습니다.
+        """,
+        responses = [
+            ApiResponse(responseCode = "200", description = "생성된 프로필 정보"),
+        ],
     )
     @PostMapping("/profile")
     fun createProfile(
@@ -50,7 +68,10 @@ class UserController(
 
     @Operation(
         summary = "내 정보 조회",
-        description = "해당하는 유저 정보를 조회합니다. 개인의 정보만 조회 가능합니다."
+        description = """### 현재 로그인한 사용자의 정보를 조회합니다.""",
+        responses = [
+            ApiResponse(responseCode = "200", description = "유저 정보"),
+        ],
     )
     @GetMapping("/me")
     fun getUserInfo(): CaramelApiResponse<GetUserInfoResponse> {
@@ -60,7 +81,15 @@ class UserController(
 
     @Operation(
         summary = "유저 설정 수정",
-        description = "수정할 설정 정보만 추가하여 보내면 됩니다. 아무것도 넣지 않을경우 현재 설정 정보를 그대로 반환합니다.",
+        description = """
+            ### 사용자의 알림설정 등을 수정합니다.
+            
+            - 수정하려는 설정 정보만 요청에 포함하면 됩니다.
+            - 수정하지 않을 값은 포함하지 않거나, `null`로 전송해야 합니다.
+        """,
+        responses = [
+            ApiResponse(responseCode = "200", description = "수정이 반영된 모든 설정 정보."),
+        ],
     )
     @PatchMapping("/settings")
     fun updateUserSetting(
@@ -72,7 +101,10 @@ class UserController(
 
     @Operation(
         summary = "유저 설정 조회",
-        description = "설정을 조회합니다.",
+        description = """### 로그인한 유저의 설정을 조회합니다.""",
+        responses = [
+            ApiResponse(responseCode = "200", description = "유저의 설정 정보"),
+        ],
     )
     @GetMapping("/settings")
     fun getUserSetting(): CaramelApiResponse<UserSettingResponse> {
