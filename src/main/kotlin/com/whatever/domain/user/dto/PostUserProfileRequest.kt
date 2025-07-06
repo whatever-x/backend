dcf2157b7908729d456d286553b045cbf445b459
given-dragon <yijy001@naver.com>
2025-07-07 00:40:56 +0900

    [Refactor] Profile 수정 Dto의 nickname validation이 User의 상수를 사용하도록 수정

--- src/main/kotlin/com/whatever/domain/user/dto/PostUserProfileRequest.kt
+++ src/main/kotlin/com/whatever/domain/user/dto/PostUserProfileRequest.kt
@@ -1,18 +1,18 @@
 package com.whatever.domain.user.dto
 
+import com.whatever.domain.user.model.User.Companion.MAX_NICKNAME_LENGTH
+import com.whatever.domain.user.model.User.Companion.MIN_NICKNAME_LENGTH
 import com.whatever.domain.user.model.UserGender
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.AssertTrue
-import jakarta.validation.constraints.NotBlank
 import jakarta.validation.constraints.Pattern
 import jakarta.validation.constraints.Size
 import java.time.LocalDate
 
 data class PostUserProfileRequest(
     @Schema(description = "닉네임")
-    @field:NotBlank(message = "닉네임은 공백일 수 없습니다.")
-    @field:Size(min = 2, max = 8, message = "닉네임은 2~8자 이내로 입력해주세요.")
-    @field:Pattern(regexp = "^[가-힣a-zA-Z0-9]+$")
+    @field:Size(min = MIN_NICKNAME_LENGTH, max = MAX_NICKNAME_LENGTH, message = "닉네임은 ${MIN_NICKNAME_LENGTH}~${MAX_NICKNAME_LENGTH}자 이내로 입력해주세요.")
+    @field:Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "닉네임은 한글, 영문, 숫자로만 입력해주세요.")
     val nickname: String,
     @Schema(description = "YYYY-MM-DD 형식의 생일")
     val birthday: LocalDate,
--- src/main/kotlin/com/whatever/domain/user/dto/PutUserProfileRequest.kt
+++ src/main/kotlin/com/whatever/domain/user/dto/PutUserProfileRequest.kt
@@ -1,15 +1,16 @@
 package com.whatever.domain.user.dto
 
+import com.whatever.domain.user.model.User.Companion.MAX_NICKNAME_LENGTH
+import com.whatever.domain.user.model.User.Companion.MIN_NICKNAME_LENGTH
 import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.Size
 import java.time.LocalDate
 
 data class PutUserProfileRequest(
     @Schema(description = "닉네임")
-    @field:Pattern(
-        regexp = "^$|[가-힣A-Za-z0-9]{2,8}$",
-        message = "닉네임은 2~8자의 한글, 영문, 숫자로만 입력해주세요."
-    )
+    @field:Size(min = MIN_NICKNAME_LENGTH, max = MAX_NICKNAME_LENGTH, message = "닉네임은 ${MIN_NICKNAME_LENGTH}~${MAX_NICKNAME_LENGTH}자 이내로 입력해주세요.")
+    @field:Pattern(regexp = "^[가-힣a-zA-Z0-9]+$", message = "닉네임은 한글, 영문, 숫자로만 입력해주세요.")
     val nickname: String?,
 
     @Schema(description = "생일")
