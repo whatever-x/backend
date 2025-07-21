5b671b3b2a062b32d337772c2afd577b34522c6e
Ahn-seokjoo <tjrwn0716@naver.com>
2025-07-22 04:44:59 +0900

    [Refactor] TestApplication 수정

--- caramel-domain/src/main/kotlin/com/whatever/domain/TestApplication.kt
+++ caramel-domain/src/main/kotlin/com/whatever/domain/TestApplication.kt
@@ -1,9 +1,11 @@
-package com.whatever
+package com.whatever.domain
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(
+    scanBasePackages = ["com.whatever.domain", "com.whatever.caramel.common"]
+)
 class TestApplication
 
 fun main(args: Array<String>) {
--- caramel-domain/src/test/kotlin/com/whatever/CaramelDomainSpringBootTest.kt
+++ caramel-domain/src/test/kotlin/com/whatever/CaramelDomainSpringBootTest.kt
@@ -1,10 +1,11 @@
 package com.whatever
 
+import com.whatever.domain.TestApplication
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.ActiveProfiles
 
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
-@SpringBootTest
+@SpringBootTest(classes = [TestApplication::class], webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @ActiveProfiles("test")
 annotation class CaramelDomainSpringBootTest
