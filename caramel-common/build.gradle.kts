87d1627f02785382fbf67dd70e4d9c8929072c11
Ahn-seokjoo <tjrwn0716@naver.com>
2025-07-21 21:35:56 +0900

    [Refactor] common build.gradle 정리

--- caramel-common/build.gradle.kts
+++ caramel-common/build.gradle.kts
@@ -3,31 +3,17 @@ import org.springframework.boot.gradle.tasks.bundling.BootJar
 group = "com.whatever.caramel-common"
 version = "0.0.1-SNAPSHOT"
 
-val opentelemetryVersion = "2.14.0"
-
 dependencies {
     // Spring
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-security")
-    // Logging
-    implementation("io.github.oshai:kotlin-logging-jvm:7.0.3")
-    implementation("org.springframework.boot:spring-boot-starter-log4j2")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-spring-boot-starter")
-    implementation("io.opentelemetry.instrumentation:opentelemetry-log4j-appender-2.17:$opentelemetryVersion-alpha")
-
-    // Jackson과 Kotlin의 호환성을 위한 모듈
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.springframework:spring-web")
+    implementation("org.springframework.data:spring-data-commons")
+    implementation("org.springframework:spring-context")
+    implementation("org.springframework.boot:spring-boot")
 
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.6")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.6")
-
-    // Test
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
 tasks.getByName<BootJar>("bootJar") {
--- caramel-common/src/main/kotlin/com/whatever/caramel/common/util/CursorUtil.kt
+++ caramel-common/src/main/kotlin/com/whatever/caramel/common/util/CursorUtil.kt
@@ -9,11 +9,11 @@ object CursorUtil {
     fun fromHash(cursorHash: String): List<String> {
         val decodedBytes = Base64.getUrlDecoder().decode(cursorHash)
         val decodedString = String(decodedBytes, StandardCharsets.UTF_8)
-        return decodedString.split(com.whatever.caramel.common.util.CursorUtil.SEPARATOR)
+        return decodedString.split(SEPARATOR)
     }
 
     fun toHash(vararg cursors: Any): String {
-        return cursors.joinToString(com.whatever.caramel.common.util.CursorUtil.SEPARATOR) { it.toString() }
+        return cursors.joinToString(SEPARATOR) { it.toString() }
             .toByteArray(StandardCharsets.UTF_8)
             .let { Base64.getUrlEncoder().withoutPadding().encodeToString(it) }
     }
