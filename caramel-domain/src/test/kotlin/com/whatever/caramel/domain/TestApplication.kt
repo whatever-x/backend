f9c563c072aa79385431d57e7d138250fe7948be
Ahn-seokjoo <tjrwn0716@naver.com>
2025-07-22 14:51:05 +0900

    [Refactor] ConfigurationPropertiesScan으로 수정

--- caramel-domain/src/test/kotlin/com/whatever/caramel/domain/TestApplication.kt
+++ caramel-domain/src/test/kotlin/com/whatever/caramel/domain/TestApplication.kt
@@ -1,20 +1,11 @@
 package com.whatever.caramel.domain
 
-import com.whatever.caramel.common.global.jwt.JwtProperties
-import com.whatever.caramel.infrastructure.properties.FirebaseProperties
-import com.whatever.caramel.infrastructure.properties.OauthProperties
-import com.whatever.caramel.infrastructure.properties.SpecialDayApiProperties
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(
-    scanBasePackages = ["com.whatever.caramel"]
-)
-@EnableConfigurationProperties(
-    JwtProperties::class, OauthProperties::class, FirebaseProperties::class,
-    SpecialDayApiProperties::class
-)
+@SpringBootApplication(scanBasePackages = ["com.whatever.caramel"])
+@ConfigurationPropertiesScan(basePackages = ["com.whatever.caramel"])
 class TestApplication
 
 fun main(args: Array<String>) {
