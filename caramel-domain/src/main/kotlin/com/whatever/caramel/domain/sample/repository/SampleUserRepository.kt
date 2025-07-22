a86d528a61179f81c699dde8f33f61eab33a06f0
Ahn-seokjoo <tjrwn0716@naver.com>
2025-07-22 14:43:35 +0900

    [Refactor] domain 위치 수정

--- caramel-domain/src/main/kotlin/com/whatever/caramel/domain/sample/repository/SampleUserRepository.kt
+++ caramel-domain/src/main/kotlin/com/whatever/caramel/domain/sample/repository/SampleUserRepository.kt
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.Profile
 import org.springframework.data.repository.CrudRepository
 
 @Profile("dev", "local-mem")
-interface SampleUserRepository : CrudRepository<com.whatever.caramel.domain.user.model.User, Long> {
-    fun findByEmailAndIsDeleted(email: String, isDeleted: Boolean = false): com.whatever.caramel.domain.user.model.User?
-    fun findByPlatformUserId(platformUserId: String): com.whatever.caramel.domain.user.model.User?
+interface SampleUserRepository : CrudRepository<User, Long> {
+    fun findByEmailAndIsDeleted(email: String, isDeleted: Boolean = false): User?
+    fun findByPlatformUserId(platformUserId: String): User?
 }
