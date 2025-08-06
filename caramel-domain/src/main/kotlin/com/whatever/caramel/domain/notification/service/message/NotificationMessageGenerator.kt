5c8e3fe85d77224039f4a9d3e25886cdff4c7f1f
given-dragon <yijy001@naver.com>
2025-08-06 22:18:43 +0900

    [Refactor] NotificationMessageGenerator 예외 로깅 통일

--- caramel-domain/src/main/kotlin/com/whatever/caramel/domain/notification/service/message/NotificationMessageGenerator.kt
+++ caramel-domain/src/main/kotlin/com/whatever/caramel/domain/notification/service/message/NotificationMessageGenerator.kt
@@ -7,6 +7,7 @@ import com.whatever.caramel.domain.notification.model.NotificationType.ANNIVERSA
 import com.whatever.caramel.domain.notification.vo.NotificationMessageVo
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Component
+import kotlin.reflect.KClass
 
 private val logger = KotlinLogging.logger {  }
 /**
@@ -17,89 +18,73 @@ interface NotificationMessageGenerator {
     fun generate(notificationMessageParameter: NotificationMessageParameter): NotificationMessageVo
 }
 
-@Component
-class MyBirthdayMessageGenerator : NotificationMessageGenerator {
-    override fun supports(): NotificationType = NotificationType.MY_BIRTHDAY
-    override fun generate(notificationMessageParameter: NotificationMessageParameter): NotificationMessageVo {
-        if (notificationMessageParameter !is BirthDayParameter) {
+abstract class AbstractNotificationMessageGenerator<T : NotificationMessageParameter>(
+    private val expectedParameterType: KClass<T>,
+) : NotificationMessageGenerator {
+    final override fun generate(notificationMessageParameter: NotificationMessageParameter): NotificationMessageVo {
+        if (!expectedParameterType.isInstance(notificationMessageParameter)) {
             logger.error {
-                "Invalid parameter type for MyBirthdayMessageGenerator. " +
-                "Expected: BirthDayParameter, " +
-                "Actual: ${notificationMessageParameter::class.simpleName}, " +
-                "parameter details: $notificationMessageParameter"
+                "Invalid parameter type for message generation. " +
+                "Generator='${this::class.simpleName}', " +
+                "Expected='${expectedParameterType.simpleName}', " +
+                "Actual='${notificationMessageParameter::class.simpleName}', " +
+                "ParameterData='${notificationMessageParameter}'"
             }
             throw InvalidMessageParameterException()
         }
 
+        @Suppress("UNCHECKED_CAST")
+        return generateMessage(notificationMessageParameter as T)
+    }
+
+    protected abstract fun generateMessage(parameter: T): NotificationMessageVo
+}
+
+@Component
+class MyBirthdayMessageGenerator : AbstractNotificationMessageGenerator<BirthDayParameter>(BirthDayParameter::class) {
+    override fun supports(): NotificationType = NotificationType.MY_BIRTHDAY
+    override fun generateMessage(parameter: BirthDayParameter): NotificationMessageVo {
         return NotificationMessageVo(
             type = supports(),
-            title = "내일은 ${notificationMessageParameter.label}일이에요!",
+            title = "내일은 ${parameter.label}일이에요!",
             body = "당신의 생일 축하축하",
         )
     }
 }
 
 @Component
-class PartnerBirthdayMessageGenerator : NotificationMessageGenerator {
+class PartnerBirthdayMessageGenerator : AbstractNotificationMessageGenerator<BirthDayParameter>(BirthDayParameter::class) {
     override fun supports(): NotificationType = NotificationType.PARTNER_BIRTHDAY
-    override fun generate(notificationMessageParameter: NotificationMessageParameter): NotificationMessageVo {
-        if (notificationMessageParameter !is BirthDayParameter) {
-            logger.error {
-                "Invalid parameter type for MyBirthdayMessageGenerator. " +
-                "Expected: BirthDayParameter, " +
-                "Actual: ${notificationMessageParameter::class.simpleName}, " +
-                "parameter details: $notificationMessageParameter"
-            }
-            throw InvalidMessageParameterException()
-        }
-
+    override fun generateMessage(parameter: BirthDayParameter): NotificationMessageVo {
         return NotificationMessageVo(
             type = supports(),
-            title = "내일은 ${notificationMessageParameter.label}일이에요!",
-            body = "${notificationMessageParameter.birthdayMemberNickname}님의 생일이니 축하해주시오",
+            title = "내일은 ${parameter.label}일이에요!",
+            body = "${parameter.birthdayMemberNickname}님의 생일이니 축하해주시오",
         )
     }
 }
 
 @Component
-class HundredAnniversaryMessageGenerator : NotificationMessageGenerator {
+class HundredAnniversaryMessageGenerator
+    : AbstractNotificationMessageGenerator<HundredAnniversaryParameter>(HundredAnniversaryParameter::class) {
     override fun supports(): NotificationType = ANNIVERSARY_HUNDRED
-    override fun generate(notificationMessageParameter: NotificationMessageParameter): NotificationMessageVo {
-        if (notificationMessageParameter !is HundredAnniversaryParameter) {
-            logger.error {
-                "Invalid parameter type for HundredAnniversaryMessageGenerator. " +
-                "Expected: HundredAnniversaryParameter, " +
-                "Actual: ${notificationMessageParameter::class.simpleName}, " +
-                "parameter details: $notificationMessageParameter"
-            }
-            throw InvalidMessageParameterException()
-        }
-
+    override fun generateMessage(parameter: HundredAnniversaryParameter): NotificationMessageVo {
         return NotificationMessageVo(
             type = supports(),
-            title = "내일은 ${notificationMessageParameter.label}일이에요!",
+            title = "내일은 ${parameter.label}일이에요!",
             body = "해피해피 데이데이"
         )
     }
 }
 
 @Component
-class YearlyAnniversaryMessageGenerator : NotificationMessageGenerator {
+class YearlyAnniversaryMessageGenerator
+    : AbstractNotificationMessageGenerator<YearlyAnniversaryParameter>(YearlyAnniversaryParameter::class) {
     override fun supports(): NotificationType = ANNIVERSARY_YEARLY
-    override fun generate(notificationMessageParameter: NotificationMessageParameter): NotificationMessageVo {
-        if (notificationMessageParameter !is YearlyAnniversaryParameter) {
-            logger.error {
-                "Invalid parameter type for YearlyAnniversaryMessageGenerator. " +
-                "Expected: YearlyAnniversaryParameter, " +
-                "Actual: ${notificationMessageParameter::class.simpleName}, " +
-                "parameter details: $notificationMessageParameter"
-            }
-            throw InvalidMessageParameterException()
-        }
-
+    override fun generateMessage(parameter: YearlyAnniversaryParameter): NotificationMessageVo {
         return NotificationMessageVo(
             type = supports(),
-            title = "내일은 ${notificationMessageParameter.label} 기념일이에요!",
+            title = "내일은 ${parameter.label} 기념일이에요!",
             body = "해피해피 데이데이"
         )
     }
