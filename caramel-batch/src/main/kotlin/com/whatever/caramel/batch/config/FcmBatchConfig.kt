1e77df70685640247d185ae1fe34dbbd55010287
Ahn-seokjoo <tjrwn0716@naver.com>
2025-08-09 16:16:30 +0900

    [Refactor] 다른쪽에서도 @BatchTransactionManager 추가

--- caramel-batch/src/main/kotlin/com/whatever/caramel/batch/config/FcmBatchConfig.kt
+++ caramel-batch/src/main/kotlin/com/whatever/caramel/batch/config/FcmBatchConfig.kt
@@ -18,7 +18,7 @@ import org.springframework.batch.item.ItemProcessor
 import org.springframework.batch.item.ItemReader
 import org.springframework.batch.item.ItemWriter
 import org.springframework.batch.item.support.ListItemReader
-import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.boot.autoconfigure.batch.BatchTransactionManager
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.transaction.PlatformTransactionManager
@@ -61,7 +61,7 @@ class FcmBatchConfig(
     @Bean
     fun anniversaryStep(
         whatEverJobRepository: JobRepository,
-        @Qualifier("batchTransactionManager") batchTransactionManager: PlatformTransactionManager,
+        @BatchTransactionManager batchTransactionManager: PlatformTransactionManager,
         anniversaryItemReader: ItemReader<ScheduledNotification>,
         compositeItemProcessor: ItemProcessor<ScheduledNotification, ScheduledNotification>,
         anniversaryItemWriter: ItemWriter<ScheduledNotification>,
