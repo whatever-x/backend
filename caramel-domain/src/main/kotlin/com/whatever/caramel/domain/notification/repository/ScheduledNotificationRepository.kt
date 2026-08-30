833b7afed05c8d1e2c1344d1858fdb8247694e66
Ahn-seokjoo <tjrwn0716@naver.com>
2026-08-31 02:20:35 +0900

    [Refactor] 미사용값 제거

--- caramel-domain/src/main/kotlin/com/whatever/caramel/domain/notification/repository/ScheduledNotificationRepository.kt
+++ caramel-domain/src/main/kotlin/com/whatever/caramel/domain/notification/repository/ScheduledNotificationRepository.kt
@@ -5,7 +5,6 @@ import com.whatever.caramel.domain.notification.model.ScheduledNotification
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import java.time.LocalDateTime
 
 interface ScheduledNotificationRepository :
